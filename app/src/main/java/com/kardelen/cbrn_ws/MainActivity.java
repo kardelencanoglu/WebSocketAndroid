c837c25a62d5098cb60357dd9bc71a48391c872d
Kardelen Canoğlu <85257891+kardelencanoglu@users.noreply.github.com>
2023-06-21 16:56:36 +0300

    cbrn ready

--- app/src/main/java/com/kardelen/cbrn_ws/MainActivity.java
+++ app/src/main/java/com/kardelen/cbrn_ws/MainActivity.java
@@ -1,32 +1,114 @@
 package com.kardelen.cbrn_ws;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import java.net.URISyntaxException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebSocketClientExample webSocketClient;
+
+    LinearLayout layoutCbrn;
+    ToggleButton cbrnBtn;
+
+    private Timer timer;
+
+    public TextView textChem, textBio, textEod, textNuc, textRad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        layoutCbrn = findViewById(R.id.cbrn_layout);
+        cbrnBtn = findViewById(R.id.cbrnBtn);
+        textChem = findViewById(R.id.textChem);
+        textBio = findViewById(R.id.textBio);
+        textEod = findViewById(R.id.textEod);
+        textNuc = findViewById(R.id.textNuc);
+        textRad = findViewById(R.id.textRad);
+
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new MyTimerTask(this), 0, 500);
+
+        cbrnBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    try {
+                        startWebSocket(buttonView);
+                    } catch (URISyntaxException e) {
+                        throw new RuntimeException(e);
+                    }
+                }else {
+                    stopWebSocket(buttonView);
+                }
+            }
+        });
+
 
         try {
-            webSocketClient = new WebSocketClientExample("ws://10.42.0.1/xip/api/device/stream");
+            webSocketClient = new WebSocketClientExample("ws://10.42.0.1/xip/api/device/stream", this);
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
+
     }
 
-    public void startWebSocket(View view) {
-        webSocketClient.connect();
+
+    private class MyTimerTask extends TimerTask {
+        private Handler handler = new Handler(Looper.getMainLooper());
+        MainActivity mainActivity;
+
+        MyTimerTask (MainActivity mainActivity){
+            this.mainActivity = mainActivity;
+        }
+        @Override
+        public void run() {
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    textChem.setText(webSocketClient.trend+"");
+                    if(webSocketClient.trend > 100){
+                        Toast.makeText(mainActivity, "TEHLİKE", Toast.LENGTH_SHORT).show();
+                    }
+                    textEod.setText(webSocketClient.ec_Value+"");
+                    textRad.setText(webSocketClient.scCell3RAbsPerBl+"");
+                    textNuc.setText(webSocketClient.flow_lpm_std+"");
+                    textBio.setText(webSocketClient.mOS2_SensorResistanceAbsPerBl+"");
+                }
+            });
+        }
     }
+
+
+    public void visibleCBRN(){
+        layoutCbrn.setVisibility(View.VISIBLE);
+    }
+
+    public void invisibleCBRN(){
+        layoutCbrn.setVisibility(View.INVISIBLE);
+    }
+
+    public void startWebSocket(View view) throws URISyntaxException {
+        if (webSocketClient == null || !webSocketClient.isOpen()) {
+            webSocketClient = new WebSocketClientExample("ws://10.42.0.1/xip/api/device/stream", this);
+            webSocketClient.connect();
+            visibleCBRN();
+        } else {
+            webSocketClient.close();
+        }
+    }
+
 
     public void stopWebSocket(View view) {
         webSocketClient.close();
+        invisibleCBRN();
     }
 }
--- app/src/main/java/com/kardelen/cbrn_ws/WebSocketClientExample.java
+++ app/src/main/java/com/kardelen/cbrn_ws/WebSocketClientExample.java
@@ -1,15 +1,29 @@
 package com.kardelen.cbrn_ws;
 
+import android.widget.Toast;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class WebSocketClientExample extends WebSocketClient {
 
-    public WebSocketClientExample(String url) throws URISyntaxException {
+    MainActivity mainActivity;
+    public float trend = 0f;
+    public float scCell3RAbsPerBl = 0f;
+    public float mOS2_SensorResistanceAbsPerBl = 0f;
+    public float ec_Value = 0f;
+    public float flow_lpm_std = 0f;
+
+    public WebSocketClientExample(String url, MainActivity mainActivity) throws URISyntaxException {
         super(new URI(url));
+        this.mainActivity = mainActivity;
     }
 
     @Override
@@ -20,6 +34,28 @@ public class WebSocketClientExample extends WebSocketClient {
     @Override
     public void onMessage(String message) {
         System.out.println("Gelen veri: " + message);
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(message);
+
+            trend = jsonNode.get("content").get("PumpPwm").asLong();
+            System.out.println("Trend value: " + trend);
+
+            scCell3RAbsPerBl = jsonNode.get("content").get("ScCell3RAbsPerBl").asLong();
+            System.out.println("scCell3RAbsPerBl value: " + scCell3RAbsPerBl);
+
+            mOS2_SensorResistanceAbsPerBl = jsonNode.get("content").get("MOS2_SensorResistanceAbsPerBl").asLong();
+            System.out.println("mOS2_SensorResistanceAbsPerBl value: " + mOS2_SensorResistanceAbsPerBl);
+
+            ec_Value = jsonNode.get("content").get("EC_Value").asLong();
+            System.out.println("ec_Value value: " + ec_Value);
+
+            flow_lpm_std = jsonNode.get("content").get("flow_lpm_std").asLong();
+            System.out.println("flow_lpm_std value: " + flow_lpm_std);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
